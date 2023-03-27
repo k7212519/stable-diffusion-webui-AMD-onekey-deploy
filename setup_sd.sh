7a5e824fc1c3cadbc080ebb0d796be2214eb70fa
k7212519 <37895779+k7212519@users.noreply.github.com>
2023-03-27 10:51:35 +0800

    Update setup_sd.sh

--- setup_sd.sh
+++ setup_sd.sh
@@ -5,7 +5,7 @@ gnome-terminal -- docker run -it --network=host --device=/dev/kfd --device=/dev/
 
 # 等待容器启动
 until [ "$(docker inspect -f '{{.State.Status}}' stable-diffusion)" = "running" ]; do
-    sleep 1
+    sleep 100
 done
 
 echo "docker is running..."
