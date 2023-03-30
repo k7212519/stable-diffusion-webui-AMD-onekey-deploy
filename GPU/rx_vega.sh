8482c212e9038381f2095bf6579e6b5d30cc16ab
k7212519 <37895779+k7212519@users.noreply.github.com>
2023-03-30 11:56:21 +0800

    Update rx_vega.sh

--- GPU/rx_vega.sh
+++ GPU/rx_vega.sh
@@ -7,4 +7,4 @@ docker ps
 docker restart stable-diffusion
 
 # 在容器内部执行命令
-docker exec -it stable-diffusion bash -c "cd /dockerx/stable-diffusion-webui && source venv/bin/activate && REQS_FILE='requirements.txt' PYTORCH_ROCM_ARCH=gfx900 HCC_AMDGPU_TARGET=gfx900 python launch.py"
+docker exec -it stable-diffusion bash -c "cd /dockerx/stable-diffusion-webui && source venv/bin/activate && REQS_FILE='requirements.txt' PYTORCH_ROCM_ARCH=gfx906 HCC_AMDGPU_TARGET=gfx906 python launch.py"
